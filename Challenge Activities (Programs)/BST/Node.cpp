69ab3f62f219c6fa4ab4f6b688c882c030b70237
JasmineLau <55225223+AppleTater@users.noreply.github.com>
2020-06-03 10:10:58 -0700

    Create Node.cpp

---
+++ Challenge Activities (Programs)/BST/Node.cpp
@@ -0,0 +1,17 @@
+#include "Node.h"
+
+Node::Node(string newData) {
+    data = newData;
+    count = 0;
+
+    left = NULL;
+    right = NULL;
+}
+
+void Node::countIncrement() {
+    count++;
+}
+
+void Node::countDecrement() {
+    count--;
+}
